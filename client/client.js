8ad3cd695f59946393e0b9876e975616bcc85919
Vitor Tomic <vitor.tomic@vicert.com>
2019-12-02 13:35:35 +0100

    implement dynamic api port

--- client/client.js
+++ client/client.js
@@ -3,10 +3,21 @@ const application = express()
 const path = require("path")
 const directory = path.join(__dirname, 'public')
 
+const apiUrl = {
+    apiUrl: null
+}
+
 application.use(express.static(directory))
 
 application.get('/', (request, response)=>{
     response.sendFile(`${directory}/view/index.html`)
 })
 
-module.exports = application
+application.get('/api-url', (request, response)=> {
+    response.json(apiUrl)
+})
+
+module.exports = {
+    application,
+    apiUrl
+}
--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const http = require('http')
 const videoDownloadApi = require('./server/server.js')
 const clientApplication = require('./client/client.js')
 
+let apiUrl = require('./client/client.js').apiUrl
+
 const videoServer = http.createServer(videoDownloadApi)
 
 videoServer.listen(0, ()=>{
     console.log(`Video processing server listening on port ${videoServer.address().port}`)
+    clientApplication.apiUrl.apiUrl = `http://localhost:${videoServer.address().port}`
 })
 
-let apiUrl = `http://localhost:${videoServer.address().port}`
-
-const clientServer = http.createServer(clientApplication)
+const clientServer = http.createServer(clientApplication.application)
 
 clientServer.listen(0, ()=>{
     console.log(`Web application server listening on port ${clientServer.address().port}`)
